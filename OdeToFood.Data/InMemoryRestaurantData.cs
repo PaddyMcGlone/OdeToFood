098cfb36cdd86b9bd583f8119cd9b16b098d018b
Paddy McGlone <paddymcglone@gmail.com>
2019-10-16 19:38:37 +0100

    Adding the InMemoryClass
    - Adding the seeded data class within project.

---
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>
+            {
+                new Restaurant{ Id = 1, Name = "Pizza the action", Cusine = CusineType.Italian, Location = "Belfast"},
+                new Restaurant{ Id = 2, Name = "Dawali", Cusine = CusineType.Indian, Location = "Belfast"},
+                new Restaurant{ Id = 3, Name = "New Mexico", Cusine = CusineType.Mexican, Location = "Belfast"}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants;
+        }
+    }
+}
