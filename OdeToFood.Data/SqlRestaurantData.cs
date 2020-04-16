ebd92150759c161f0554d52f4649ae45327b14fd
Paddy McGlone <paddymcglone@gmail.com>
2020-04-16 19:09:05 +0100

    Adding the new Restaurant data file.

---
+++ OdeToFood.Data/SqlRestaurantData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        public SqlRestaurantData()
+        {
+        }
+
+        public Restaurant Add(Restaurant NewRestaurant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Restaurant DeleteRestaurant(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Restaurant FindRestaurant(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Restaurant> FindRestaurants(string name = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Restaurant Update(Restaurant UpdatedRestaurant)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
