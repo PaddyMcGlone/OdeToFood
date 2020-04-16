0a54ec45f04f22a8c38bb559f492a4b72895f31d
Paddy McGlone <paddymcglone@gmail.com>
2020-04-16 20:06:36 +0100

    Adding the new files.

---
+++ OdeToFood/Pages/Restaurants/Delete.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class DeleteModel : PageModel
+    {
+        public Restaurant restaurant { get; set; }
+
+        public IRestaurantData RestaurantData { get; }
+
+        public DeleteModel(IRestaurantData restaurantData)
+        {
+            RestaurantData = restaurantData;
+        }        
+
+        public IActionResult OnGet(int id)
+        {
+        }
+
+        public IActionResult OnPost(int id)
+        {
+
+        }
+    }
+}
